15508e33654a08eba5589b43b063ecd5dec9c64d
NarendraprasathgJLK <narendraprasath@jlk-tech.co.in>
2024-10-01 10:14:48 +0530

    Add Heroku settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -16,6 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+import django_heroku
+django_heroku.settings(locals())
 
 
 # Quick-start development settings - unsuitable for production
